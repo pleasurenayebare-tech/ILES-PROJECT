d24cad8323d2adb88518f1bb54bf7ad3489e3831
annanambi10-commits <annanambi10@gmail.com>
2026-05-13 10:54:44 -0700

    Update models.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -14,7 +14,7 @@ class CustomUser(AbstractUser):
     department = models.CharField(max_length=150, blank=True, null=True)
     staff_number = models.CharField(max_length=50, blank=True, null=True, unique=True)
     student_number = models.CharField(max_length=50, blank=True, null=True, unique=True)
-    phone_number = models.CharField(max_length=20, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return f"{self.get_full_name() or self.username} ({self.role})"
