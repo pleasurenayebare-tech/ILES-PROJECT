4ea5d7573c9b72ba4a4e19aa0a9f009a8fd68175
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-17 02:33:13 -0700

    Update fields in NotificationSerializer
    Fix the fields declaration to use a variable instead of '__all__'.

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -6,5 +6,5 @@ from .models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = "__all__"
+        fields
         read_only_fields = ("user", "created_at")
