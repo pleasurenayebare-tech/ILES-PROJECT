1bf438e5d1c71d1385a0671357b8ae9a3161b088
annanambi10-commits <annanambi10@gmail.com>
2026-05-16 21:46:41 -0700

    Update models.py

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -5,7 +5,8 @@ User = settings.AUTH_USER_MODEL
 
 
 class Notification(models.Model):
-    CHANNEL_CHOICES= [
+    CHANNEL_CHOICES= [ 
+        ("in_app" , "In_app"),
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
     title = models.CharField(max_length=160)
     message = models.TextField()
