a1add06db47f13ff6ad15e21fe29d72c7734c78d
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-17 02:37:34 -0700

    Add 'createdat' field to NotificationSerializer

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -6,5 +6,5 @@ from .models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ("id","title","message","channel","isread")
+        fields = ("id","title","message","channel","isread","createdat")
         read_only_fields = ("user", "created_at")
