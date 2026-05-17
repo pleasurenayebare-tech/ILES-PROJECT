af0c9a492b9162acc9bd634ac8ed97a14bde6963
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-17 02:36:34 -0700

    Add 'isread' field to NotificationSerializer

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -6,5 +6,5 @@ from .models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ("id","title","message","channel")
+        fields = ("id","title","message","channel","isread")
         read_only_fields = ("user", "created_at")
