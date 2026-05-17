c3051a9e3261ca8e166b5911ac2ff2b02a65dfda
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-17 02:47:33 -0700

    Fix formatting of fields in NotificationSerializer

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -6,5 +6,5 @@ from .models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ("id", "title", "message","channel","is_read","created_at")
+        fields = ("id", "title", "message", "channel","is_read","created_at")
         read_only_fields = ("created_at",)
