70b3a82aa26ff3159fcc7dbc2fa44d004d944dc6
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-17 02:48:43 -0700

    Remove unnecessary blank line in serializers.py

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 
 from .models import Notification
 
-
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
