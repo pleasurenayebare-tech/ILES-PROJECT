15a8b01a153e439623b0a64943c1d525e05309df
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-17 23:45:06 -0700

    Add comment for Django database settings reference
    Added a comment with a link to the Django documentation for database settings.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
-
+# https://docs.djangoproject.com/en/6.0/ref/settings/
 DATABASES = {
     "default": dj_database_url.parse(
         os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
