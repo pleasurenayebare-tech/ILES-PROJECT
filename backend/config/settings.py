617ade8d560bfaad039b60455cbe926ff211ca31
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-17 23:36:51 -0700

    Update database settings in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -67,7 +67,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     "default": dj_database_url.parse(
