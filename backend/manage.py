e7ad2ae1070f2bfa56be46356c999555b13a47ea
annanambi10-commits <annanambi10@gmail.com>
2026-05-18 07:46:27 -0700

    Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -10,6 +10,7 @@ def main():
     -runserver  : Start the development server. 
     -makemigrations/migrate : Handle database migrations
     -createsuperuser : Create an admin user
+    -shell : Open an interactive Python shell with Django loaded 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
   #Import Django's CLI execution function 
