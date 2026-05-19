4f405fd1299fc0a69a96b771152e4ee36594d13a
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-18 23:59:07 -0700

    Clarify STATIC_URL setting with a comment
    Added comment to clarify STATIC_URL setting.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True  # Store datetime and timezone-aware in the database
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static/"  # URL prefix for static files
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
