d8f423821a2102fd81654ac41abf537985270479
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-21 10:07:20 -0700

    Add app name configuration for Accounts app

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -6,5 +6,6 @@ class AccountsConfig(AppConfig):
     
     # Default feild type for auto-generated primary keys
     default_auto_field = "django.db.models.BigAutoField"
+    
     # The name of the app as recognised by Django
     name = 'accounts'
