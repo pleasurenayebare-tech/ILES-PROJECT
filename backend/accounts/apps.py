5407a9c93e101e1233b2e36f75779885fe138892
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-21 10:05:18 -0700

    Update apps.py

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     """Configuration class for the Accounts application."""
     # Default feild type for auto-generated primary keys
-    
+    default_auto_field = django.db.models.BigAutoField
     name = 'accounts'
