32014b3ff949ec00922b7f59cf663b479d3dbef3
annanambi10-commits <annanambi10@gmail.com>
2026-05-23 08:58:58 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken 
 
 from .serializers import RegisterSerializer, UserSerializer
 from . permissions import IsAdminRole
