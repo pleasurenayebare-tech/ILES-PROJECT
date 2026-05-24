8054056988ec30258726bc76a8f69883172b4c82
annanambi10-commits <annanambi10@gmail.com>
2026-05-23 20:04:22 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -26,6 +26,7 @@ class ChangePasswordView(APIView):
           old_password = request.data.get("old_password")
           new_password = request.data.get("new_password")
 if not old_password or not new_password:
-return Response({"error": "Both old and new password are required."}, status=status.HTTP_400_BAD_REQUEST
+return Response(
+       {"error": "Both old and new password are required."}, status=status.HTTP_400_BAD_REQUEST
 )
 
