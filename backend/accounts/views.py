33121b0baafd997e01dba872b96c71cff0d76168
annanambi10-commits <annanambi10@gmail.com>
2026-05-23 19:21:52 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -4,4 +4,4 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken 
 from .serializers import RegisterSerializer , UserSerializer 
-from . permissions import IsAdminRole
+from .permissions import IsAdminRole
