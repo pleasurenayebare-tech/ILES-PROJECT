d6acc69484e76a85510c62abcb49be6dee65418a
annanambi10-commits <annanambi10@gmail.com>
2026-05-23 19:21:03 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,3 +3,4 @@ from rest_framework import generics , permission ,status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken 
+from .serializers import RegisterSerializer , UserSerializer 
