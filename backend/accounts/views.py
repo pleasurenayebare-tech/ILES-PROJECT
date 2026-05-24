7417c478acce379b3e456182b8e6f27964193a62
annanambi10-commits <annanambi10@gmail.com>
2026-05-23 19:19:00 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,4 @@
 from django.contrib.auth import get_user_model,update_session_auth_hash
 from rest_framework import generics , permission ,status
 from rest_framework.response import Response
+from rest_framework.views import APIView
