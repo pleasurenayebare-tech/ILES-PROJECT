4034879aeb947110ec853aed3a8397a325294c84
desreealuga767-ux <desreealuga767@gmail.com>
2026-05-25 07:22:20 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -40,5 +40,6 @@ class ChangePasswordView(APIView):
 class UserListView(generics.ListAPIView):
     serializer_class = UserSerializer
     permission_classes = [IsAdminRole]
+def get_queryset(self)
 class LogoutView(APIView):
     permission _classes = [permissions.IsAuthenticated]
