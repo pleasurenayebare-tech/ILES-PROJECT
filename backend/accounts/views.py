a9e80a74446454fb029406d18da1ab5036cccaef
annanambi10-commits <annanambi10@gmail.com>
2026-05-25 12:42:10 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -13,7 +13,7 @@ class Registerview(generics.CreateAPIView):
 class CurrentUserView(APIView):
       permission_classes =    [permissions.IsAunthenticated]
       def get(self, request):
-      
+serializers=Userserializer(request.user)
           
 class UpdateProfileView(generics.UpdateAPIView):
       serializer_class = UserSerializer 
