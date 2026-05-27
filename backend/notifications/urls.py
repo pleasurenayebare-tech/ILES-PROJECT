d62c096fbfa3f6532b708d4caa1a6cc6364fa0d3
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-27 08:15:12 -0700

    Update urls.py

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import NotificationViewSet
 
+
 router = DefaultRouter()
 router.register("", NotificationViewSet, basename="notifications")
 
