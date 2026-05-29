c53bf3f764a5ca9f8ff53a8f9ec73964796622ba
annanambi10-commits <annanambi10@gmail.com>
2026-05-29 08:03:46 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -23,6 +23,7 @@ class Registerview(generics.CreateAPIView):
 
 class CurrentUserView(APIView):
     """
+API endpoint to retrieve the currently authenticated user's profile.
    
       permission_classes =    [permissions.IsAunthenticated]
 
