3a095c9ac253f898851cd31c3a933fbb70e40f68
annanambi10-commits <annanambi10@gmail.com>
2026-05-29 08:10:11 -0700

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -28,7 +28,7 @@ class CurrentUserView(APIView):
 
       def get(self, request):
           """
-Handle GET requests.
+          Handle GET requests.
           serializers = Userserializer(request.user)
           return Response(serializer.data)
           
