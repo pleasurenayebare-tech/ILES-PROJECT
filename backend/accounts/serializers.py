2e8040a04d28ecea8146ec266cece8ffd9a385a9
queen709 <deminingalin@gmail.com>
2026-05-30 18:16:58 +0300

    Update serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -10,7 +10,7 @@ def validate(self, attrs):
     if role in ("WorkplaceSupervisor", "AcademicSupervisor") and not attrs.get("staff_number"):
         raise serializers.ValidationError({"staff_number": "Required for supervisors."})
         
-    return attrs
+    return 
 
 class RegisterSerializer(serializers.ModelSerializer):
     """Serialiser for handling new user registration."""
