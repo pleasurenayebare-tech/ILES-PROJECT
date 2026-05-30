9b14aec7f6efdf5b7af68a9deb59c6344bde6025
annanambi10-commits <annanambi10@gmail.com>
2026-05-30 08:21:39 -0700

    Update urls.py

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 
 from .views import NotificationViewSet
 
-
 # GET     /api/notifications/     - list all notifications for the current user
 # GET     /api/notifications/{id}/       - retrieve a single notification
 # PATCH   /api/notifications/{id}/mark_read/     - mark a notification as read
