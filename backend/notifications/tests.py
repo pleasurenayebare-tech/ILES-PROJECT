ae18b220f6ec6ac168f7eb1ab8a90e5152794068
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-05-31 10:35:37 -0700

    Update tests.py

--- backend/notifications/tests.py
+++ backend/notifications/tests.py
@@ -11,4 +11,4 @@ class NotificationsModelTest(TestCase):
     """Test cases for the Notification model."""
   
     def setUp(self):
-        """Set up a test user before each test runs.
+        """Set up a test user before each test runs."""
