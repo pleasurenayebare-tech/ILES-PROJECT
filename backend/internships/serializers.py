8f82c8f30ee02dfe5ae8fe291e3523593069e05e
queen709 <deminingalin@gmail.com>
2026-06-01 17:09:38 +0300

    Update serializers.py

--- backend/internships/serializers.py
+++ backend/internships/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 from .models import Evaluation, EvaluationCriteria, InternshipPlacement, WeeklyLog, WeeklyLogState
 
-User = get_user_model()
+User = get_user_model(
 
 
 class InternshipPlacementSerializer(serializers.ModelSerializer):
