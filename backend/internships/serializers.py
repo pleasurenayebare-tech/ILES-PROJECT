862c053c81d2456dfed84c4aa4464af1c92b03a4
queen709 <deminingalin@gmail.com>
2026-06-01 17:13:53 +0300

    Update serializers.py

--- backend/internships/serializers.py
+++ backend/internships/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-frommodels import Evaluation, EvaluationCriteria, InternshipPlacement, WeeklyLog, WeeklyLogState
+from .models import Evaluation, EvaluationCriteria, InternshipPlacement, WeeklyLog, WeeklyLogState
 
 User = get_user_model()
 
