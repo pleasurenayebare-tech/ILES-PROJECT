32ccb26b9c0e509b809bb19e18f30ae835cd2c7f
queen709 <deminingalin@gmail.com>
2026-06-01 17:11:44 +0300

    Update serializers.py

--- backend/internships/serializers.py
+++ backend/internships/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
+fromrest_framework import serializers
 
 from .models import Evaluation, EvaluationCriteria, InternshipPlacement, WeeklyLog, WeeklyLogState
 
