2cb8884e0c8774c6207a7701c3f794e098cfd437
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-06-03 20:57:15 -0700

    Update apps.py

--- backend/notifications/apps.py
+++ backend/notifications/apps.py
@@ -6,5 +6,6 @@ class NotificationsConfig(AppConfig):
 
     # Default field type for auto-generated primary keys
     default_auto_field = "django.db.models.BigAutoField"
+
     
     name = 'notifications'
