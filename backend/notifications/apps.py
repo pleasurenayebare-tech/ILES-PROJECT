e464b2f6aaf7fff6208bf4c5f9f3ed172a943168
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-06-03 20:58:14 -0700

    Clarify app name in notifications app config
    Added comment to clarify app name in Django.

--- backend/notifications/apps.py
+++ backend/notifications/apps.py
@@ -7,5 +7,5 @@ class NotificationsConfig(AppConfig):
     # Default field type for auto-generated primary keys
     default_auto_field = "django.db.models.BigAutoField"
 
-    
+    # The name of the app as recognised by Django
     name = 'notifications'
