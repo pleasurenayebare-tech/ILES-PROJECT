f696961a89c35f73cc430a0fe358406d8e5cce60
queen709 <deminingalin@gmail.com>
2026-06-04 08:00:52 +0300

    Update models.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -13,7 +13,7 @@ class CustomUser(AbstractUser):
     role = models.CharField(max_length=32, choices=UserRole.choices, default=UserRole.STUDENT)
     department = models.CharField(max_length=150, blank=True, null=True)
     staff_number = models.CharField(max_length=50, blank=True, null=True, unique=True)
-    student_number = models.CharField(max_length=50, blank=True, null=True, unique=True
+    student_number = models.CharField(max_length=50, blank=True, null=True, unique=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     def save(self, *args, **kwargs):
