c713fe0757eb91d04758a636154a9f8e3d0a9a40
queen709 <deminingalin@gmail.com>
2026-06-04 08:20:16 +0300

    Update serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -2,7 +2,7 @@
     """Cross-field validation to enforce role-specifc required fields."""
     role = attrs.get("role")
     
-    # students must provide a students numbe
+    # students must provide a students number
     if role == "Student" and not attrs.get("student_number"):
         raise serializers.ValidationError({"student_number": "Required for students."})
         
