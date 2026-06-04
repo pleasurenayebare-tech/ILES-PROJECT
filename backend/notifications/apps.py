0d69650cc5e6fc3f38bff2755c7b886ac23438fd
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-06-03 20:53:03 -0700

    Fix docstring formatting in NotificationsConfig

--- backend/notifications/apps.py
+++ backend/notifications/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NotificationsConfig(AppConfig):
-    """Configuration class for the Notifications application.
+    """Configuration class for the Notifications application."""
     name = 'notifications'
