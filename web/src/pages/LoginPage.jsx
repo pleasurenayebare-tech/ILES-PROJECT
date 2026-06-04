f2038bbce2ee515f7c165127a799609bd9a78b71
annanambi10-commits <annanambi10@gmail.com>
2026-06-04 00:00:46 -0700

    Update LoginPage.jsx

--- web/src/pages/LoginPage.jsx
+++ web/src/pages/LoginPage.jsx
@@ -10,6 +10,7 @@ export default function LoginPage() {
   const navigate = useNavigate();
 
   useEffect(() => {
+if (user) navigate("/");
 
   const submit = async (e) => {
     e.preventDefault();
