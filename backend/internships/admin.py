33547feb167230aa4ed63d3bb89c908cb049d7f3
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-06-06 11:35:57 -0700

    Change registration method for InternshipPlacement

--- backend/internships/admin.py
+++ backend/internships/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Evaluation, EvaluationCriteria, InternshipPlacement, WeeklyLog
 
-admin.site.register(InternshipPlacement)
+@admin.register(InternshipPlacement)
 admin.site.register(WeeklyLog)
 admin.site.register(EvaluationCriteria)
 admin.site.register(Evaluation)
