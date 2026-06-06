efef38ffaa7b183641b394fc1d739cb71eff2cc1
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-06-06 11:38:06 -0700

    Update admin.py

--- backend/internships/admin.py
+++ backend/internships/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Evaluation, EvaluationCriteria, InternshipPlacement, WeeklyLog
 
 @admin.register(InternshipPlacement)
-
+class InternshipPlacementAdmin(admin.ModelAdmin):
 
 
