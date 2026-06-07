1eee624fa46b134029b71043fad6bdc120132174
annanambi10-commits <annanambi10@gmail.com>
2026-06-06 23:18:21 -0700

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import CurrentUserView, RegisterView
 
 urlpatterns = [
-    # Register a new user account
+    
     path("register/", RegisterView.as_view(), name="register"),
 
     # Login and receive access + refresh JWT tokens
