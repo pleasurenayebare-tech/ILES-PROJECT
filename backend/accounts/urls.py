b6f88d6c095718b7f57846ce0ae12e196875c6af
annanambi10-commits <annanambi10@gmail.com>
2026-06-06 23:20:03 -0700

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
 
-    
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
     # Get the currently authenticated user's profile
