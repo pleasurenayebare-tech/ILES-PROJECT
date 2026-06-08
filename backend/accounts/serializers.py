21ab8d0096d5c53029124a77e634af462bf78b47
queen709 <deminingalin@gmail.com>
2026-06-08 10:10:28 +0300

    Update serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_mode
 
 User = get_user_model()
 
