cd16d797aa1c69f243e269e26dac97d36e91af90
queen709 <deminingalin@gmail.com>
2026-06-08 10:13:20 +0300

    Update serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User(
-        fields = 
+        fields 
             "id", "username", "email", "first_name", "last_name",
             "role", "student_number", "staff_number", "phone_number", "department"
         )
