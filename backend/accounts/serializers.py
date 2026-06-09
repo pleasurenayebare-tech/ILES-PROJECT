d3315071afd00aeb37aeb4ab757500bdda2d8ad4
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-06-08 22:26:35 -0700

    Update serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -57,7 +57,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a new user with an encrypted password."""
         password = validated_data.pop("password")
-        
+
+        # Remove empty strings for unique fields to avoid constraint errors
         user = User(**validated_data)
         user.set_password(password)
         user.save()
