1cccad77730fbd0c331f7f3cb38e85e318e4c13b
desreealuga767-ux <desreealuga767@gmail.com>
2026-06-10 02:58:51 -0700

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-# TokenObtainPairView: handles login and returns access + refresh tokens
+
 # TokenRefreshView: handles refreshing an expired access token
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 # Custom views defined in this app's views.py
