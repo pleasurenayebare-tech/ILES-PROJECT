283bded0e6a890021447579c923f25852850573c
queen709 <deminingalin@gmail.com>
2026-06-10 16:48:43 +0300

    Update tests.py

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -16,7 +16,7 @@ class AccountsModelTest(TestCase):
         )
 
     def test_user_created_successfully(self):
-        """Test that a user is created with the correct usernam."""
+        """Test that a user is created with the correct username."""
         self.assertEqual(self.student.username, "teststudent")
 
     def test_user_email(self):
