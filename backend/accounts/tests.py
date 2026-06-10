8355a88d20fd84246eb4183b2a7fd64e56d7300b
queen709 <deminingalin@gmail.com>
2026-06-10 16:44:22 +0300

    Update tests.py

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class AccountsModelTest(TestCase):
-    """Test cases for the Accounts app models.""
+    """Test cases for the Accounts app models."""
     
     def setUp(self):
         """Set up test data before each test runs."""
