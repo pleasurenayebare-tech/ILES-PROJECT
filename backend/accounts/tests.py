dbb21694e2998e96ee25c1afc4263166988d21af
queen709 <deminingalin@gmail.com>
2026-06-10 16:44:36 +0300

    Update tests.py

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -6,7 +6,7 @@ User = get_user_model()
 class AccountsModelTest(TestCase):
     """Test cases for the Accounts app models."""
     
-    def setUp(self):
+    def setUp(self)
         """Set up test data before each test runs."""
         self.student = User.objects.create_user(
             username="teststudent",
