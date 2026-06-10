099d0963783c69eb3a9af488b836684673e28597
queen709 <deminingalin@gmail.com>
2026-06-10 16:43:30 +0300

    Update tests.py

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-User = get_user_model(
+User = get_user_model()
 
 class AccountsModelTest(TestCase):
     """Test cases for the Accounts app models."""
