6fa3c74e721c4d31cf4605710dea74166e57853a
queen709 <deminingalin@gmail.com>
2026-06-10 16:45:24 +0300

    Update tests.py

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -8,7 +8,7 @@ class AccountsModelTest(TestCase):
     
     def setUp(self):
         """Set up test data before each test runs."""
-        self.student = User.objects.create_user(
+        self.student = User.objects.create_user
             username="teststudent",
             email="student@test.com",
             password="Test1234!",
